9b6caacc80def278a695a695ad5cb10db96f666d
apiszczukw <apiszczukw@gmail.com>
2024-11-06 16:53:59 +0100

    Add project files.

---
+++ ProjektPortfolio_sr1630/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektPortfolio_sr1630.Models;
+using System.Diagnostics;
+
+namespace ProjektPortfolio_sr1630.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ ProjektPortfolio_sr1630/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjektPortfolio_sr1630.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
